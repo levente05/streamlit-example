c30efa15812e7c46c73f88456be6d7b7a8b8f3b2
LorinczLevente <90178603+levente05@users.noreply.github.com>
2023-11-30 13:59:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,13 @@
 import streamlit as st
+import pandas as pd
 
-from src.agstyler import PINLEFT, PRECISION_TWO, draw_grid
+# Példa adatok létrehozása
+data = {'Név': ['John', 'Jane', 'Bob', 'Alice'],
+        'Életkor': [25, 30, 22, 28],
+        'Város': ['New York', 'San Francisco', 'Los Angeles', 'Chicago']}
 
-formatter = {
-    'player_name': ('Player', PINLEFT),
-    'team': ('Team', {'width': 80}),
-    'poss': ('Posessions', {'width': 110}),
-    'mp': ('mp', {'width': 80}),
-    'raptor_total': ('RAPTOR', {**PRECISION_TWO, 'width': 100}),
-    'war_total': ('WAR', {**PRECISION_TWO, 'width': 80}),
-    'pace_impact': ('Pace Impact', {**PRECISION_TWO, 'width': 120})
-}
+df = pd.DataFrame(data)
 
-row_number = st.number_input('Number of rows', min_value=0, value=20)
-data = draw_grid(
-    df.head(row_number),
-    formatter=formatter,
-    fit_columns=True,
-    selection='multiple',  # or 'single', or None
-    use_checkbox='True',  # or False by default
-    max_height=300
-)
+# Streamlit alkalmazás kódja
+st.title('Példa táblázat Streamlit-ben')
+st.dataframe(df)
