cdf27f12c4aaf5316b5d40ac78d31469afacc829
LorinczLevente <90178603+levente05@users.noreply.github.com>
2023-11-30 13:53:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,23 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-df = pd.DataFrame(
-   np.random.randn(10, 5),
-   columns=('col %d' % i for i in range(1, 5)))
+from src.agstyler import PINLEFT, PRECISION_TWO, draw_grid
 
-st.table(df)
+formatter = {
+    'player_name': ('Player', PINLEFT),
+    'team': ('Team', {'width': 80}),
+    'poss': ('Posessions', {'width': 110}),
+    'mp': ('mp', {'width': 80}),
+    'raptor_total': ('RAPTOR', {**PRECISION_TWO, 'width': 100}),
+    'war_total': ('WAR', {**PRECISION_TWO, 'width': 80}),
+    'pace_impact': ('Pace Impact', {**PRECISION_TWO, 'width': 120})
+}
+
+row_number = st.number_input('Number of rows', min_value=0, value=20)
+data = draw_grid(
+    df.head(row_number),
+    formatter=formatter,
+    fit_columns=True,
+    selection='multiple',  # or 'single', or None
+    use_checkbox='True',  # or False by default
+    max_height=300
+)
