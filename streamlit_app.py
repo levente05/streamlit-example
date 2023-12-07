b5cc2abbfb032d564bc8d3ef7136c5e2744d65de
LorinczLevente <90178603+levente05@users.noreply.github.com>
2023-12-07 08:23:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+"""import streamlit as st
 import pandas as pd
 
 data = {'Név': ['John', 'Jane', 'Bob', 'Alice'],
@@ -9,4 +9,10 @@ df = pd.DataFrame(data)
 
 # Streamlit alkalmazás kódja
 
-st.dataframe(df)
+st.dataframe(df)"""
+
+
+def show_contact_form():
+
+if user send contact form == True:
+         def upload_file_function():
